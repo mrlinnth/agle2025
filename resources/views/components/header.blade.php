c01c37d3b6d9ea5c60b7562e7bf5e5552e2ad0ad
Yan <yanlinnthor@evolxit.com>
2025-02-11 11:22:34 +0630

    fix mobile menu

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -106,24 +106,21 @@
 
                 <div class="block lg:hidden">
                     <div class=" relative inline-block text-left dropdown">
-                        <button class="rounded-sm bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
+                        <button
+                            class="burger rounded-sm bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
                             <svg xmlns="http://www.w3.org/2000/svg" class="size-5" fill="none" viewBox="0 0 24 24"
                                 stroke="currentColor" stroke-width="2">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
                             </svg>
                         </button>
                         <div
-                            class="opacity-0 invisible dropdown-menu transition-all duration-300 transform origin-top-right -translate-y-2 scale-95">
-                            <div class="absolute right-0 w-56 mt-2 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none"
-                                aria-labelledby="headlessui-menu-button-1" id="headlessui-menu-items-117"
-                                role="menu">
-                                <div class="py-1">
-                                    @foreach ($menuItems as $item)
-                                        <a class="flex justify-between w-full px-4 py-2 text-sm leading-5 text-left hover:text-primary"
-                                            role="menuitem" href="{{ $item['path'] }}">
-                                            {{ $item['title'] }} </a>
-                                    @endforeach
-                                </div>
+                            class="links hidden absolute right-0 w-56 mt-2 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none">
+                            <div class="py-1">
+                                @foreach ($menuItems as $item)
+                                    <a class="flex justify-between w-full px-4 py-2 text-sm leading-5 text-left hover:text-primary"
+                                        role="menuitem" href="{{ $item['path'] }}">
+                                        {{ $item['title'] }} </a>
+                                @endforeach
                             </div>
                         </div>
                     </div>
