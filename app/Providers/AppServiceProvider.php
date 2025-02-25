50740d6d13d8b721559c943431124b16a7f5a6db
yan <yanlinnthor@evolxit.com>
2025-02-25 08:53:09 +0630

    update service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,12 +22,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $setting = Setting::firstOrFail();
-        View::share('name', $setting->name);
-        View::share('logo', $setting->logo);
-        View::share('noti_email', $setting->noti_email);
-        View::share('contact', $setting->contact);
-        View::share('countdown', $setting->countdown);
-        View::share('center_logos', $setting->footer['center_logos']);
-        View::share('end_logo', $setting->footer['end_logo']);
+        if ($setting) {
+            View::share('name', $setting->name);
+            View::share('logo', $setting->logo);
+            View::share('noti_email', $setting->noti_email);
+            View::share('contact', $setting->contact);
+            View::share('countdown', $setting->countdown);
+            View::share('center_logos', $setting->footer['center_logos']);
+            View::share('end_logo', $setting->footer['end_logo']);
+        }
     }
 }
